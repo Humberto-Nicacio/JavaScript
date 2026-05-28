cc9dd3ca71aaf2ab6b419eaecce17e3889ff3f85
Humberto Nicácio <betonm96@gmail.com>
2026-05-28 15:58:18 -0300

    estudando repetições pelo node

---
+++ aula/aula14/ambiente2.js
@@ -0,0 +1,14 @@
+/* 
+var c = 1 (inicio)
+while (c <= 5) (teste) {
+    console.log(c)
+    c++ (incr)
+}
+*/
+// - - - - - - - - - - -
+
+console.log('Vai começar...')
+for(var c = 1;c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
